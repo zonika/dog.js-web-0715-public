2819bc47e020336d6145b87c9a6a73938bb0163e
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-10-29 15:56:55 -0400

    adds more tests

--- spec/Dog.spec.js
+++ spec/Dog.spec.js
@@ -3,38 +3,50 @@
 describe('Dog', function() {
   var dog;
   beforeEach(function() {
-    dog = new Dog();
+    dog = new Dog('Buttons');
     dog.age = 5;
   });
 
-  describe("#age", function() {
-    it("should return an Integer", function(){
+  describe('name property', function() {
+    it('should have a name stored on creation in a property', function() {
+      expect(dog.name).toEqual('Buttons');
+    });
+
+    it('should not share a name between to instances', function() {
+      var piper = new Dog('Piper');
+      expect(piper.name).toEqual('Piper');
+      expect(dog.name).toEqual('Piper');
+    });
+  });
+
+  describe('#age', function() {
+    it('should return an Integer', function(){
       expect(typeof dog.age).toEqual('number');
     });
 
-    it("should be able to be set/get", function(){
+    it('should be able to be set/get', function(){
       expect(dog.age).toEqual(5);
     });
   });
 
-  describe("#ageInYears", function() {
-    it("should return an Integer", function(){
+  describe('#ageInYears', function() {
+    it('should return an Integer', function(){
       expect(typeof dog.ageInYears()).toEqual('number');
     });
 
-    it("should return age", function() {
+    it('should return age', function() {
       expect(dog.ageInYears()).toEqual(5);
     });
   });
 
-  describe("#ageInDays", function() {
-    it("should return the dog's age in days", function(){
+  describe('#ageInDays', function() {
+    it('should return the dog\'s age in days', function(){
       expect(dog.ageInDays()).toEqual(1825);
     });
   });
 
-  describe("#ageInDogYears", function() {
-    it("should return the dog's age in dog years", function(){
+  describe('#ageInDogYears', function() {
+    it('should return the dog\'s age in dog years', function(){
       expect(dog.ageInDogYears()).toEqual(35);
     });
   });
